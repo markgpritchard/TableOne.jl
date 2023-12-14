38f74be2cdc1ed483ef3f993fc7208c147b27442
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2023-12-14 17:36:16 +0000

    add tests, and ability to cope with different variable types

--- src/TableOne.jl
+++ src/TableOne.jl
@@ -5,10 +5,17 @@ using DataFrames, StatsBase
 
 export tableone
 
-function tableone(data, strata, vars; 
-        binvars = Symbol[], catvars = Symbol[], npvars = Symbol[], 
-        addnmissing = true, addtotal = true, kwargs...
-    )
+"""
+    tableone(data, strata[, vars]; <keyword arguments>)
+
+Produce a summary table which may be used as Table 1 in a manuscript.
+
+If `vars` is not supplied, defaults to use all variables in the dataset.
+"""
+function tableone(data, strata, vars::Vector{S}; 
+        binvars = S[], catvars = S[], npvars = S[], 
+        addnmissing = true, addtotal = false, kwargs...
+    ) where S 
     stratanames = unique(getproperty(data, strata))
     strataids = Dict{Symbol}{Vector{Int}}()
     table1 = DataFrame()
@@ -38,25 +45,64 @@ function tableone(data, strata, vars;
     return table1
 end
 
-function tableone!(table1, data, strata, stratanames, strataids, vars, binvars, catvars, npvars; 
-        kwargs...
-    )
+# In case only one variable is supplied, rather than a vector
+tableone(data, strata, var) = tableone(data, strata, [ var ])
+
+# If `vars` is not supplied, defaults to use all variables in the dataset
+tableone(data, strata; kwargs...) = tableone(data, strata, Symbol.(names(data)); kwargs...)
+
+# Variables can be listed as Symbols or Strings, but need to be consistent. Functions 
+# to check this consistency and make sure all lists are vectors
+
+function tableone!(table1, data, strata, stratanames, strataids, vars::Vector{S}, 
+        binvars, catvars, npvars::S; kwargs...
+    ) where S
+    return tableone!(table1, data, strata, stratanames, strataids, vars, binvars, catvars, [ npvars ]; 
+        kwargs...)
+end
+
+function tableone!(table1, data, strata, stratanames, strataids, vars::Vector{S}, 
+        binvars, catvars::S, npvars::Vector{S}; kwargs...
+    ) where S
+    return tableone!(table1, data, strata, stratanames, strataids, vars, binvars, [ catvars ], npvars; 
+        kwargs...)
+end
+
+function tableone!(table1, data, strata, stratanames, strataids, vars::Vector{S}, 
+        binvars::S, catvars::Vector{S}, npvars::Vector{S}; kwargs...
+    ) where S
+    return tableone!(table1, data, strata, stratanames, strataids, vars, [ binvars ], catvars, npvars; 
+        kwargs...)
+end
+
+function tableone!(table1, data, strata, stratanames, strataids, vars::Vector{S}, 
+        binvars::Vector{S}, catvars::Vector{S}, npvars::Vector{S}; kwargs...
+    ) where S
     for v ∈ vars
         append!(
-                table1, 
-                newvariable(data, strata, stratanames, strataids, v, binvars, catvars, npvars; 
-                    kwargs...)
-            )
+            table1, 
+            newvariable(data, strata, stratanames, strataids, v, binvars, catvars, npvars; 
+                kwargs...)
+        )
     end
 end
 
-function newvariable(data, strata, stratanames, strataids, v, binvars, catvars, npvars; varnames = nothing, kwargs...)
+function newvariable(data, strata, stratanames, strataids, v, binvars, catvars, npvars; 
+        varnames = nothing, kwargs...
+    )
     varvect = getproperty(data, v)
     varname = getvarname(v, varnames)
+    return newvariable(v, strata, stratanames, strataids, varvect, varname, binvars, catvars, npvars; 
+        kwargs...)
+end
+
+function newvariable(v, strata, stratanames, strataids, varvect, varname, binvars, catvars, npvars; 
+        kwargs...
+    )
     if v ∈ catvars 
         return catvariable(strata, stratanames, strataids, varvect, varname; kwargs...)
     elseif v ∈ binvars
-        return binvariable(strata, stratanames, strataids, varvect, varname; kwargs...)
+        return binvariable(v, strata, stratanames, strataids, varvect, varname; kwargs...)
     elseif v ∈ npvars
         return npvariable(strata, stratanames, strataids, varvect, varname; kwargs...)
     else 
@@ -67,7 +113,7 @@ end
 function catvariable(strata, stratanames, strataids, varvect, varname; 
         addnmissing, addtotal, kwargs...
     )
-    levels = skipmissing(unique(varvect))
+    levels = skipmissing(sort(unique(varvect)))
     _t = DataFrame()
     variablenames::Vector{String} = [ [ varname ]; [ "    $lev" for lev ∈ levels ] ]
     insertcols!(_t, :variablenames => variablenames)
@@ -103,10 +149,10 @@ function catvariable!(estimates::Vector{String}, varvect, level, stratumids, tot
     push!(estimates, "$(sprint(show, n)) ($(sprint(show, round(pc; digits))))")
 end
 
-function binvariable(strata, stratanames, strataids, varvect, varname; 
-        addnmissing, addtotal, varnames = nothing, kwargs...
+function binvariable(v, strata, stratanames, strataids, varvect, varname; 
+        addnmissing, addtotal, varnames = nothing, binvardisplay = nothing, kwargs...
     )
-    level = maximum(skipmissing(unique(varvect)))
+    level = binvariabledisplay(v, varvect, binvardisplay)
     _t = DataFrame()
     variablenames::Vector{String} = [ "$varname: $level" ]
     insertcols!(_t, :variablenames => variablenames)
@@ -152,19 +198,29 @@ function meanvariable(strata, stratanames, strataids, varvect, varname; kwargs..
 end
 
 function meanvariable!(_t, varvect, ids, sn; digits = 1, kwargs...)
-    mn = mean(varvect[ids])
-    sd = std(varvect[ids])
+    mn = mean(skipmissing(varvect[ids]))
+    sd = std(skipmissing(varvect[ids]))
     _mn = "$(sprint(show, round(mn; digits)))" 
     _sd = "$(sprint(show, round(sd; digits)))"
-    estimates::Vector{String} = [ "$_med ($_iqr)" ]
+    estimates::Vector{String} = [ "$_mn ($_sd)" ]
     insertcols!(_t, Symbol(sn) => estimates)
+end
+
+function autovariable(strata, stratanames, strataids, varvect::AbstractVector, varname; kwargs...)
+    return autovariable(strata, stratanames, strataids, Array(varvect), varname; kwargs...)
 end
 
 function autovariable(strata, stratanames, strataids, varvect::Vector{<:Number}, varname; kwargs...)
     return meanvariable(strata, stratanames, strataids, varvect, varname; kwargs...)
 end
 
-function autovariable(strata, stratanames, strataids, varvect::Vector{<:String}, varname; kwargs...)
+function autovariable(strata, stratanames, strataids, varvect::Vector{S}, varname; 
+        kwargs...
+    ) where S <:Union{<:Number, Missing}
+    return meanvariable(strata, stratanames, strataids, varvect, varname; kwargs...)
+end
+
+function autovariable(strata, stratanames, strataids, varvect::Vector, varname; kwargs...)
     return catvariable(strata, stratanames, strataids, varvect, varname; kwargs...)
 end
 
@@ -200,6 +256,14 @@ function addnmissing!(_t, varvect)
     nmissing = [ "" for _ ∈ axes(_t, 1) ]
     nmissing[1] = sprint(show, n)
     insertcols!(_t, :nmissing => nmissing)
+end
+
+binvariabledisplay(v, varvect, binvardisplay::Nothing) = maximum(skipmissing(unique(varvect)))
+
+function binvariabledisplay(v, varvect, binvardisplay::Dict)
+    if v ∈ keys(binvardisplay) return binvardisplay[v]
+    else                       return maximum(skipmissing(unique(varvect)))
+    end 
 end
 
 end # module TableOne
